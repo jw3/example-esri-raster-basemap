c69eb147d0658c1b0239ed8149a918fa4e97a9ef
John Wass <jwass3@gmail.com>
2019-06-04 05:11:38 -0400

    add mapview

--- app/src/main/java/com/github/jw3/rasterbasemap/MainActivity.kt
+++ app/src/main/java/com/github/jw3/rasterbasemap/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.github.jw3.rasterbasemap
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.geometry.SpatialReferences
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView.map = ArcGISMap(Basemap.Type.IMAGERY, 42.0, 42.0, 4)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val LocationPermissionRequest = 111
+        private val SR = SpatialReferences.getWgs84()
     }
 }
